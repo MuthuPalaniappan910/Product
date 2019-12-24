2b44a243e299e5a9ba567023eb9e4f6319034aa4
MuthuPalaniappan910 <palaniappantce@gmail.com>
2019-12-24 07:17:08 +0530

    Integrate with email

--- src/test/java/com/ecommerce/product/service/ProductServiceTest.java
+++ src/test/java/com/ecommerce/product/service/ProductServiceTest.java
@@ -13,9 +13,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
 import com.ecommerce.product.dto.BuyRequestDto;
 import com.ecommerce.product.dto.ProductList;
